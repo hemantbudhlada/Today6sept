5162191d1a4d3106d732707af5518555b3125334
hanumant <hemantguys@gmail.com>
2022-09-06 09:28:18 +0530

    commit to my branch

---
+++ demo/src/mypackage/SampleClass2.java
@@ -0,0 +1,11 @@
+package mypackage;
+
+public class SampleClass2
+{
+	public static void main(String[] args) {
+		
+		System.out.println("Heant kumar is best");
+		System.out.println("line 2");
+	}
+
+}
